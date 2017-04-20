f7738b552cd404c2ef29b09a64824e607840f33d
wostlund <wostlund@stuy.edu>
2017-04-20 12:11:43 -0400

    Added new and improved file

--- hw.py
+++ hw.py
@@ -1,4 +1,4 @@
 import math
 print [x for x in range(2, 101) if x % 2 == 0]
 print [y for y in range(2, 101) if y % 3 == 0]
-print [z for z in range(2, 101) if ( z in [z if (z % h == 0 and z != h) else 0 for h in range(2, max(3, int(round(math.sqrt(z) + 1)))) ] ) ]
+print [z for z in range(2, 101) if ( z in [z for h in range(2, max(3, int(round(math.sqrt(z) + 1)))) if z % h == 0 and z != h] ) ]
